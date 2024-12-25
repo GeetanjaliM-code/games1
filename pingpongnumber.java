85b626ca14169ea7a209ac8667f05e9914b0ec7c
GeetanjaliM-code <geetanjalisony2003@gmail.com>
2024-12-25 23:20:31 +0530

    Create pingpongnumber.java

---
+++ pingpongnumber.java
@@ -0,0 +1,10 @@
+public class PingPongGame {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) System.out.println("PingPong");
+            else if (i % 3 == 0) System.out.println("Ping");
+            else if (i % 5 == 0) System.out.println("Pong");
+            else System.out.println(i);
+        }
+    }
+}
